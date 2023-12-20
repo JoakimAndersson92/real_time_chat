7ee2622fb40e6d4ecd7959fb94f9e3c4ed0c518d
joakimAndersson92 <joakim.andersson920202@gmail.com>
2023-12-20 15:32:26 +0100

    Add middleware to handle static style and images in express

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+//Express dont allow serving static files without the use of static middleware
+app.use('/style',express.static(__dirname + '/style'));
+app.use('/images',express.static(__dirname + '/images'));
+
 app.get("/", function (req, res) {
   res.render('index', {
     title: "Real Time Chat"
